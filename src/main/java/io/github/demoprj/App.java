cec7ed7660c8700deba1a69a188d14a74f5247a7
tokoyi <tokoyi@gmail.com>
2024-03-17 15:30:01 +0800

    Initial commit

---
+++ src/main/java/io/github/demoprj/App.java
@@ -0,0 +1,11 @@
+package io.github.demoprj;
+
+/**
+ * Hello maven package
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello maven package ...");
+    }
+}
